87d959a999366dbf340220fe89bce58232c0cd4d
lmihailovic <miha.luka1006@gmail.com>
2026-01-05 00:49:14 +0100

    prvi zadatak iz listi

---
+++ 1-liste/zad1/reverse.cpp
@@ -0,0 +1,23 @@
+struct ListNode
+{
+    int val;
+    struct ListNode *sledeci;
+};
+
+ListNode* okreniListu(ListNode* glava)
+{
+    ListNode* trenutni = glava;  // glava liste
+    ListNode* prosli = NULL;    // pokazivac za prethodni element
+    ListNode* sledeci = NULL;   // pokazivac na sledeci elemenet
+
+    while(trenutni)             // prolazak kroz listu (dokle god trenutni nije NULL)
+    {
+        sledeci = trenutni->sledeci;    // pamtimo sledeci element koji moramo da posetimo
+        trenutni->sledeci = prosli;     // trenutni element stavljamo da pokazuje na prethodni
+        prosli = trenutni;              // trenutni postaje prethodni za sledeci prolaz petlje
+        trenutni = sledeci;             // pomeramo se na sledeci element
+    }
+
+    glava = prosli;
+    return glava;
+}
