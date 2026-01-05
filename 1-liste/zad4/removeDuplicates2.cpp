9b645e2aadc1260baaf3072c810850a6093eaf37
lmihailovic <miha.luka1006@gmail.com>
2026-01-05 22:35:55 +0100

    addede zad4

--- 1-liste/zad4/removeDuplicates2.cpp
+++ 1-liste/zad4/removeDuplicates2.cpp
@@ -1,3 +1,47 @@
-//
-// Created by lule on 5.1.26..
-//
+struct ListNode
+{
+    int vrednost;
+    struct ListNode *sledeci;
+};
+
+
+ListNode* obrisiTrenutniCvor(ListNode* glava, ListNode* prethodni, ListNode* trenutni, ListNode* sledeci)
+{
+    if (prethodni == NULL)  // ako je trenutni cvor glava, ne moze da ima prethodni...
+        glava = sledeci;    // ...time je nova glava sledeci cvor
+    else
+        prethodni->sledeci = sledeci;   // "preskacemo" trenutni cvor
+    // free(trenutni)       // rucno oslobadjamo memoriju za trenutni i obrisani cvor
+    return glava;           // vracamo glavu na listu
+}
+
+ListNode* ukloniDuplikate(ListNode* glava)
+{
+    ListNode* prethodni = NULL;
+    ListNode* trenutni = glava;
+    ListNode* sledeci = NULL;
+
+    bool jesuIsti = false;  // oznacava da li su dva uzastopna cvora isti
+
+    while (trenutni) {
+        sledeci = trenutni->sledeci;
+
+        // ako su trenutni i sledeci cvor isti, brisemo trenutni
+        if (sledeci && trenutni->vrednost == sledeci->vrednost) {
+            jesuIsti = true;
+            glava = obrisiTrenutniCvor(glava, prethodni, trenutni, sledeci);
+        } else if (jesuIsti) {
+
+            // ako su prethodna dva cvora bili jednaki, onda je trenutni cvor
+            // poslednji preostali duplikat, te i njega brisemo
+            jesuIsti = false;
+            glava = obrisiTrenutniCvor(glava, prethodni, trenutni, sledeci);
+        } else {
+            jesuIsti = false;
+            prethodni = trenutni;
+        }
+        trenutni = sledeci;
+    }
+
+    return glava;
+}
