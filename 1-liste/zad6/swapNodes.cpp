2122bc3a95d61d2631e7833a4c0fc558c7bf801a
lmihailovic <miha.luka1006@gmail.com>
2026-01-05 23:25:38 +0100

    wip version of zad6

---
+++ 1-liste/zad6/swapNodes.cpp
@@ -0,0 +1,38 @@
+struct ListNode
+{
+    int vrednost;
+    struct ListNode *sledeci;
+};
+
+ListNode* zameniParove(ListNode* glava)
+{
+    if (glava == NULL || glava->sledeci == NULL)
+        return glava;
+
+    ListNode* prethodni = NULL;
+    ListNode* trenutni = glava;
+    ListNode* sledeci = glava->sledeci;
+
+    glava = glava->sledeci;
+
+    while (sledeci) {
+        if (prethodni)                          // ako prethodni postoji...
+            prethodni->sledeci = sledeci;       // ...sledeci od prethodnog pokazuje na sledeci
+
+        trenutni->sledeci = sledeci->sledeci;   // sledeci postaje cvor nakon sledeceg
+        sledeci->sledeci = trenutni;            // cvor nakon sledeceg postaje trenutni
+
+        //
+
+
+        prethodni = trenutni;                   // prethodni postaje trenutni
+        trenutni = trenutni->sledeci;           // trenutni postaje sledeci
+
+        if (trenutni)
+            return glava;
+
+        sledeci = trenutni->sledeci;
+    }
+
+    return glava;
+}
