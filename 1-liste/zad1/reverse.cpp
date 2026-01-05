49c8313c4c6aed38a9674744d4ccfe18122bfdd7
lmihailovic <miha.luka1006@gmail.com>
2026-01-05 02:54:53 +0100

    minor formatting in zad1

--- 1-liste/zad1/reverse.cpp
+++ 1-liste/zad1/reverse.cpp
@@ -10,7 +10,7 @@ ListNode* okreniListu(ListNode* glava)
     ListNode* prosli = NULL;    // pokazivac za prethodni element
     ListNode* sledeci = NULL;   // pokazivac na sledeci elemenet
 
-    while(trenutni) {           // prolazak kroz listu (dokle god trenutni nije NULL)
+    while (trenutni) {          // prolazak kroz listu (dokle god trenutni nije NULL)
 
         sledeci = trenutni->sledeci;    // pamtimo sledeci element koji moramo da posetimo
         trenutni->sledeci = prosli;     // trenutni element stavljamo da pokazuje na prethodni
