02a5aca041de82801d611b890bedbe880aa67e35
lmihailovic <miha.luka1006@gmail.com>
2026-01-06 16:57:19 +0100

    added todo in zad6

--- 1-liste/zad6/swapNodes.cpp
+++ 1-liste/zad6/swapNodes.cpp
@@ -15,15 +15,14 @@ ListNode* zameniParove(ListNode* glava)
 
     glava = glava->sledeci;
 
+    // TO DO: objasniti slikovitoi i korak po korak donji kod
+
     while (sledeci) {
         if (prethodni)                          // ako prethodni postoji...
             prethodni->sledeci = sledeci;       // ...sledeci od prethodnog pokazuje na sledeci
 
         trenutni->sledeci = sledeci->sledeci;   // sledeci postaje cvor nakon sledeceg
         sledeci->sledeci = trenutni;            // cvor nakon sledeceg postaje trenutni
-
-        //
-
 
         prethodni = trenutni;                   // prethodni postaje trenutni
         trenutni = trenutni->sledeci;           // trenutni postaje sledeci
