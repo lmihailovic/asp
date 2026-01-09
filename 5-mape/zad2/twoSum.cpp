65fbd1fe029a984f4b5680f94bd0f299eb23f63f
lmihailovic <miha.luka1006@gmail.com>
2026-01-10 00:02:20 +0100

    added zad2

--- 5-mape/zad2/twoSum.cpp
+++ 5-mape/zad2/twoSum.cpp
@@ -1,3 +1,20 @@
-//
-// Created by lule on 9.1.26..
-//
+vector<int> twoSum(vector<int>& niz, int target)
+{
+    // [key: value] nam je sada
+    // [vrednost: indeks]
+    unordered_map<int, int> mapa;
+
+    // klasicna for petlja, jer nam treba indeks 'i'
+    for (int i = 0; i < niz.size(); i++) {
+
+        auto drugiSabirak = mapa.find(target - niz[i]); // trazimo da li postoji drugi sabirak
+                                                        // tako da je niz[i] + drugiSabirak == target
+
+        if (drugiSabirak != mapa.end())                 // ::find() vraca ::map.end() ako ne nadje element
+            return {drugiSabirak->first, i};
+
+        mapa[niz[i]] = i;
+    }
+
+    return {0, 0};
+}
