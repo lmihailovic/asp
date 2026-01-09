55aa124c3a58dee67b30805d4129e013c8ea91db
lmihailovic <miha.luka1006@gmail.com>
2026-01-09 19:30:08 +0100

    added maps and zad1 in maps

---
+++ 5-mape/zad1/containsDuplicates.cpp
@@ -0,0 +1,15 @@
+bool sadrziDuplikate(vector<int>& niz)
+{
+    // inicijalizujemo praznu mapu...
+    // sa kljucem int, i vrednoscu bool za svaki kljuc
+    unordered_map<int, bool> mapa;
+
+    // ekvivalentno petlji: foreach element in niz
+    for (int element : niz) {
+        if (mapa[element])      // ako za kljuc "element" imamo vrednost true,
+            return true;        // znaci da smo vec videli taj element => duplikat
+
+        mapa[element] = true;   // prvi put vidimo ovaj element => dodajemo ga u mapu
+    }
+    return false;
+}
