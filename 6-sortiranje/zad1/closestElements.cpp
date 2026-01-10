7cec55247c9e6426b21f866e08c00c2ec8af897c
lmihailovic <miha.luka1006@gmail.com>
2026-01-11 00:58:08 +0100

    added zad1

--- 6-sortiranje/zad1/closestElements.cpp
+++ 6-sortiranje/zad1/closestElements.cpp
@@ -1,3 +1,16 @@
-//
-// Created by lule on 11.1.26..
-//
+pair<int, int> najbliziElementi(vector<int> niz)
+{
+    sort(niz.begin(), niz.end());
+
+    int najmanjaRazlika = a[1] - a[0];
+    pair <int, int> resenje = {a[0], a[1]};
+
+    for (int i = 2; i < niz.size(); i++) {
+        if (a[i] - a[i - 1] < najmanjaRazlika) {
+            najmanjaRazlika = a[i] - a[i - 1];
+            resenje = {a[i - 1], a[i]};
+        }
+    }
+
+    return resenje;
+}
