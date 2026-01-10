83d530ef545bb28bc47623571124d4cf3e343838
lmihailovic <miha.luka1006@gmail.com>
2026-01-11 00:58:14 +0100

    added zad2

--- 6-sortiranje/zad2/podelaCokoladica.cpp
+++ 6-sortiranje/zad2/podelaCokoladica.cpp
@@ -1,3 +1,23 @@
-//
-// Created by lule on 11.1.26..
-//
+int podelaCokoladica(vector<int> paketi, int k)
+{
+    int n = paketi.size();
+
+    sort(paketi.begin(), paketi.end());
+
+    int najmanjaRazlika = -1;
+    int levo = 0;
+    int desno = k - 1;
+
+    while (levo < desno) {
+        // najveca razlika ce uvek biti izmedju prvog i poslednjeg paketa u opsegu
+        // zato nas i ona jedino interesuje,
+        // tj. gde je razlika izmedju pocetka i kraja opsega najmanja
+        najmanjaRazlika = min(najmanjaRazlika, paketi[desno] - paketi[levo]);
+
+        // pomeramo opseg
+        levo += 1;
+        desno += 1;
+    }
+
+    return najmanjaRazlika;
+}
