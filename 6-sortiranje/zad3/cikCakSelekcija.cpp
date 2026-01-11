59591a978ca4bae22991b1c7198627b13c719f56
lmihailovic <miha.luka1006@gmail.com>
2026-01-11 10:21:07 +0100

    added zad3 from sort

--- 6-sortiranje/zad3/cikCakSelekcija.cpp
+++ 6-sortiranje/zad3/cikCakSelekcija.cpp
@@ -1,3 +1,20 @@
-//
-// Created by lule on 11.1.26..
-//
+vector<int> cikCakSelekcija(vector<int> niz)
+{
+    sort(niz.begin(), niz.end());
+    vector<int> resenje;
+
+    int levo = 0;
+    int desno = niz.size() - 1;
+
+    for (int i = 0; i < niz.size(); i++) {
+        if (i % 2 == 0) {
+            resenje[levo] = niz[i];
+            levo += 1;
+        } else {
+            resenje[desno] = niz[i];
+            desno -= 1;
+        }
+    }
+
+    return resenje;
+}
